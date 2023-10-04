ddeb72d8c010b1a4bc6bc40109ba008c6f06525e
Conner Wattles <connerwattles@gmail.com>
2023-10-04 11:10:57 -0600

    Working circular doubly linked list convex hull

--- convex_hull.py
+++ convex_hull.py
@@ -65,12 +65,14 @@ class ConvexHullSolver(QObject):
 		assert( type(points) == list and type(points[0]) == QPointF )
 
 		t1 = time.time()
-		sortedPoints = sorted(points, key=lambda k: [k[0], k[1]])
+		sortedPoints = sorted(points, key=lambda k: k.x())
 		t2 = time.time()
 
 		t3 = time.time()
 		# this is a dummy polygon of the first 3 unsorted points
-		polygon = [QLineF(points[i],points[(i+1)%3]) for i in range(3)]
+		#polygon = [QLineF(points[i],points[(i+1)%3]) for i in range(3)]
+		node = divide_and_conquer(sortedPoints)
+		polygon = convert_to_qlinef_list(node)
 		# TODO: REPLACE THE LINE ABOVE WITH A CALL TO YOUR DIVIDE-AND-CONQUER CONVEX HULL SOLVER
 		t4 = time.time()
 
@@ -79,42 +81,115 @@ class ConvexHullSolver(QObject):
 		self.showHull(polygon,RED)
 		self.showText('Time Elapsed (Convex Hull): {:3.3f} sec'.format(t4-t3))
 
-	def divide_and_conquer(points):
-		n = len(points)
-		if n <= 1:
-			return points
-		left = divide_and_conquer(points[:n//2])
-		right = divide_and_conquer(points[n//2:])
-		return merge(left, right)
+def divide_and_conquer(points):
+	n = len(points)
+	if n <= 1:
+		hull = create_doubly_linked_list(points)
+		return hull
+	left = divide_and_conquer(points[:n//2])
+	right = divide_and_conquer(points[n//2:])
+	
+	lpu, rpu, lpl, rpl = find_tangents(left, right)
 
-	def merge(left, right):
-		if not left or not right:
-			return left or right
-		if left[-1][0] <= right[0][0]:
-			lower_tangent = merge_lower_tangent(left, right)
-		else:
-			lower_tangent = merge_lower_tangent(right, left)
-		if left[0][0] >= right[-1][0]:
-			upper_tangent = merge_upper_tangent(left, right)
-		else:
-			upper_tangent = merge_upper_tangent(right, left)
-		return lower_tangent + upper_tangent[1:-1] + lower_tangent
+	lpu.next = rpu
+	rpu.prev = lpu
+	rpl.next = lpl
+	lpl.prev = rpl
 
-	def merge_lower_tangent(left, right):
-		while len(left) > 1 and len(right) > 1:
-			if slope(left[-2], left[-1]) <= slope(right[0], right[1]):
-				left.pop()
-			else:
-				right.pop(0)
-		return left + right
+	return lpu
 
-	def merge_upper_tangent(left, right):
-		while len(left) > 1 and len(right) > 1:
-			if slope(left[0], left[1]) >= slope(right[-2], right[-1]):
-				left.pop(0)
-			else:
-				right.pop()
-		return left + right
+def find_tangents(left, right):
 
-	def slope(p, q):
-		return (q[1] - p[1]) / (q[0] - p[0])
+	leftHullRightMost = find_rightmost_node(left)
+	rightHullLeftMost = find_leftmost_node(right)
+
+	leftTangentUpperPoint = leftHullRightMost
+	rightTangentUpperPoint = rightHullLeftMost
+	done = True
+	while done:
+		done = False
+		previousSlope = slope(leftTangentUpperPoint.point, rightTangentUpperPoint.point)
+
+		while slope(leftTangentUpperPoint.prev.point, rightTangentUpperPoint.point) < previousSlope:
+			leftTangentUpperPoint = leftTangentUpperPoint.prev
+			previousSlope = slope(leftTangentUpperPoint.point, rightTangentUpperPoint.point)
+			done = True
+
+		while slope(leftTangentUpperPoint.point, rightTangentUpperPoint.next.point) > previousSlope:
+			rightTangentUpperPoint = rightTangentUpperPoint.next
+			previousSlope = slope(leftTangentUpperPoint.point, rightTangentUpperPoint.point)
+			done = True
+
+	leftTangentLowerPoint = leftHullRightMost
+	rightTangentLowerPoint = rightHullLeftMost
+	done = True
+	while done:
+		done = False
+		previousSlope = slope(leftTangentLowerPoint.point, rightTangentLowerPoint.point)
+
+		while slope(leftTangentLowerPoint.next.point, rightTangentLowerPoint.point) > previousSlope:
+			leftTangentLowerPoint = leftTangentLowerPoint.next
+			previousSlope = slope(leftTangentLowerPoint.point, rightTangentLowerPoint.point)
+			done = True
+
+		while slope(leftTangentLowerPoint.point, rightTangentLowerPoint.prev.point) < previousSlope:
+			rightTangentLowerPoint = rightTangentLowerPoint.prev
+			previousSlope = slope(leftTangentLowerPoint.point, rightTangentLowerPoint.point)
+			done = True
+
+	return leftTangentUpperPoint, rightTangentUpperPoint, leftTangentLowerPoint, rightTangentLowerPoint
+
+
+def slope(p1, p2):
+	return (p2.y() - p1.y()) / (p2.x() - p1.x())
+
+
+class Node:
+	def __init__(self, point):
+		self.point = point
+		self.prev = None
+		self.next = None
+
+def create_doubly_linked_list(points):
+	# Create a doubly linked list from a list of points
+	head = Node(points[0])
+	tail = head
+	for point in points[1:]:
+		node = Node(point)
+		node.prev = tail
+		tail.next = node
+		tail = node
+	head.prev = tail
+	tail.next = head
+	return head
+
+def convert_to_qlinef_list(head):
+	# Convert a doubly linked list of QPointF objects to a list of QLineF objects
+	qlinef_list = []
+	node = head
+	while True:
+		qlinef_list.append(QLineF(node.point, node.next.point))
+		node = node.next
+		if node == head:
+			break
+	return qlinef_list
+
+def find_rightmost_node(head):
+	# Find the rightmost node in a circular doubly linked list
+	node = head
+	rightmost_node = node
+	while node.next != head:
+		node = node.next
+		if node.point.x() > rightmost_node.point.x():
+			rightmost_node = node
+	return rightmost_node
+
+def find_leftmost_node(head):
+	# Find the leftmost node in a circular doubly linked list
+	node = head
+	leftmost_node = node
+	while node.next != head:
+		node = node.next
+		if node.point.x() < leftmost_node.point.x():
+			leftmost_node = node
+	return leftmost_node
